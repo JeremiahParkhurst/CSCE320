07edde8f95a892f220e68afd3e316a8cdbf2174a
John Doan <doanja@plu.edu>
2015-05-10 12:46:43 -0700

    Java Docs.
    GameViewBoard.java
    GameViewBoard.form

--- Gomoku/src/gomoku/GameViewBoard.java
+++ Gomoku/src/gomoku/GameViewBoard.java
@@ -12,8 +12,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
- *
- * @author John
+ * The GameViewBoard displays the Gomoku game board that consists of JButtons
+ * that will change color depending on the player's input.
  */
 public class GameViewBoard extends JPanel{
     private int row, col;
@@ -24,12 +24,16 @@ public class GameViewBoard extends JPanel{
     private final int MAX_COUNT = 1; //maximum number of moves
     private int count = 0; //current number of moves
     
+    /**
+     * Constructor, initializes the GameViewBoard
+     * @param con, the GameViewController
+     */
     public GameViewBoard(GameViewController con){
         gcon = con;
         row = boardSize;
         col = boardSize;
-    
-        //set up grid in for the center panel
+        
+        //Sets up grid
         this.setLayout(new GridLayout(row,col));
         square = new MyJButton[row][col];
         SquareListener listener = new SquareListener();
@@ -44,47 +48,55 @@ public class GameViewBoard extends JPanel{
                 this.add(square[i][j]);
             }
         }
-   
     }
-    /**
-     * changes cell color indicating the cell has a gomoku piece
-     * @param i
-     * @param j 
-     */ 
-    private void updateCell(int i, int j){
-         
-     }
     
     /**
-     * sends grid to opponent vice versa
+     * Changes cell color indicating the cell has a gomoku piece
+     * @param i
+     * @param j
+     */
+    private void updateCell(int i, int j){
+        // TODO add your handling code here:
+    }
+    
+    /**
+     * Updates the board, sends the grid to opponent, vice versa
      */
     public void updateGrid() {
-        
+        // TODO add your handling code here:
     }
     
     /**
      * The actionPerformed method is called each time a button
-     * in the center panel is selected. It allows the user to
-     * select MAX_COUNT	WHITE cells to inoculate (set to GREEN)
-     *
+     * on the GameViewBoard selected. It allows the user to select 
+     * MAX_COUNT greater or equal to 1 boardColor cells to send to 
+     * the opposing player.
+     * 
+     * Cell Color Definitions:
+     * boardColor = empty cell (valid location regardless)
+     * black = taken cell (by this player)
+     * white = taken cell (by the opposing player)
+     * yellow = tentative cell (indicates the player has chosen this cell
+     * this turn)
      */
     private class SquareListener implements ActionListener{
         
         public void actionPerformed(ActionEvent e) {
             MyJButton button = (MyJButton) e.getSource();
             
-            //deselect (check needed to deselect previous legal moves)
+            //deselects (check needed to deselect previous legal moves)
             if(button.getBackground().equals(Color.YELLOW)){
                 button.setBackground(boardColor);
                 count = 0;
                 gcon.clearStatus();
                 gcon.moveDeselected();
             }
+            
             //if the user makes a move that is not an open space
             else if(button.getBackground().equals(Color.WHITE) || button.getBackground().equals(Color.BLACK)){
                 gcon.clearStatus();
                 gcon.invalidMoveMsg();
-            }   
+            }
             //the move is successful (no cell is not marked by a player)
             else if(MAX_COUNT != count){
                 button.setBackground(Color.YELLOW);
@@ -96,25 +108,23 @@ public class GameViewBoard extends JPanel{
     }
     
     /**
-     * The actionPerformed method is called each time the step
-     * button in the north panel is selected. The user must have
-     * select MAX_COUNT	WHITE cells to inoculate (set to GREEN)
-     *
+     * The actionPerformed method is called each time the sendMove
+     * button is selected. The user must have select MAX_COUNT = 1
+     * in order to send the move. (MAX_COUNT indicates the number of moves
+     * they have made this turn.
      */
     private class ButtonListener implements ActionListener{
-        
         public void actionPerformed(ActionEvent e) {
-            
-        }    
+            // TODO add your handling code here:
+        }
     }
     
     /**
-     * This is a private helper class the extends
-     * JButton to include its (i,j) location in the
-     * center panel.
+     * This is a private helper class the extends JButton to include
+     * its (i,j) location in the center panel.
      */
     private static class MyJButton extends JButton{
-        private int i;
-        private int j;
+        private int i; //the row
+        private int j; //the column
     }
 }
